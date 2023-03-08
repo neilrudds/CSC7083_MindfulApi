c0ecaba0ba5d66a77b6a4afae794ba2ba4d54364
neilrudds <neilrudds@hotmail.com>
2023-03-08 20:09:22 +0000

    Added header to enable client side headers. This resolved issue with client authorization Bearer token.

--- src/app.js
+++ src/app.js
@@ -30,6 +30,7 @@ app.use(bodyParser.json())
 
 app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Headers', '*')
     next();
 })
 
