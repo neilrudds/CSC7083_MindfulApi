fd0fe28ccbe5f4e1c07ca4c97561edb6b0dba728
neilrudds <neilrudds@hotmail.com>
2023-03-09 15:55:05 +0000

    Login password is now base64 encoded from client side. Login controller updated to decode the base64 string.

--- src/v1/controllers/loginController.js
+++ src/v1/controllers/loginController.js
@@ -7,9 +7,14 @@ const login = async (req, res) => {
         console.log("".concat("[Method: ", req.method, "], [Url: ", req.url, "], [Body: ", JSON.stringify(req.body), "]"));
         const { username, password } = req.body;
 
+        // Decode base64 password
+        const buff = Buffer.from(password, 'base64');
+        const decoded_password = buff.toString('utf-8');
+
+        // Locate the user in the db
         let user = await loginService.getOneUser(username);
         
-        if (username == user[0].username && bcrypt.compareSync(password, user[0].hashed_password)) {
+        if (username == user[0].username && bcrypt.compareSync(decoded_password, user[0].hashed_password)) {
             var token = jwt.sign({ username: username }, 'secretkey', (err, token) => {
                 res.send({
                    ok: true,
